aef774c2ec58ab30938b9c6a73fb82f0f94c30ff
DEVIN SWEENEY - STUDENT <g00407454@atu.ie>
2022-11-07 09:46:38 +0000

    Initial commit-Start

---
+++ src/main/java/UsingCatches.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UsingCatches {
+    public static void main(String[] args){
+        System.out.println("Please print a number between 0-9 ");
+        Scanner myscan = new Scanner(System.in);
+        int myNum = myscan.nextInt();
+
+        if (myNum = myscan.nextInt();
+        System.out.print("You entered "+ myNum);
+
+    else
+    {
+        System.out.print("Not valid number");
+
+
+    }
+
+
+}
